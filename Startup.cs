3d98360b53f46e4c2341783c640e986cabca3675
Madhusudhan G <madhu.gms7@gmail.com>
2017-07-10 16:05:16 -0500

    Added Logging Mechanism

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using AspNetCore_RestApiWithDapper.Middleware;
+using Swashbuckle.AspNetCore.Swagger;
+using AspNetCore_RestApiWithDapper.Business;
+using AspNetCore_RestApiWithDapper.DataRespository;
 
 namespace AspNetCore_RestApiWithDapper
 {
@@ -27,17 +31,38 @@ namespace AspNetCore_RestApiWithDapper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             // Add framework services.
             services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IProductBusiness, ProductBusiness>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ASP.NET Core RESTful API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddFile("C:/@Logs/aspnetcore-rest-api-with-dapper-{Date}.txt");
 
+            app.UseMiddleware(typeof(GlobalExceptionMiddleware));
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ASP.NET Core RESTful API v1");
+            });
         }
     }
 }
